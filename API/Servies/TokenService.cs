d7777edea535e26a28a087365b531ab67ba5c6a1
Ven Ra <73223734+venra0712@users.noreply.github.com>
2024-08-01 15:16:55 +0800

    End of Section 4

---
+++ API/Servies/TokenService.cs
@@ -0,0 +1,38 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Servies
+{
+    public class TokenService(IConfiguration config) : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access TokenKey from App Settings");
+            if (tokenKey.Length < 64) throw new Exception("Your token needs to be longer");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+            
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, user.UserName)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);    
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
