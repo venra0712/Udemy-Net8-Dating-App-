8d352b2e11aa08aa8da785abdd08e8533de0f148
VenRa <fravenj69@gmail.com>
2024-08-04 12:27:39 +0800

    End of Section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,16 @@ import {provideAnimations} from '@angular/platform-browser/animations'
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
+    
   ]
 };
