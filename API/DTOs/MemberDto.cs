4c224ec2a1403106c6c8ee330b8947861a6176fe
Ven Ra <73223734+venra0712@users.noreply.github.com>
2024-08-05 15:37:15 +0800

    End of Section 9

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -6,7 +6,6 @@ namespace API.DTOs
         public string? Username { get; set; }
         public int Age { get; set; }
         public string? PhotoUrl { get; set; }
-        public DateOnly DateOfBirth { get; set; }
         public string? KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -10,24 +9,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  http = inject(HttpClient);
   registerMode = false;
-  users: any;
-
-  ngOnInit() : void {
-    this.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has been completed.')
-    })
   }
 
   cancelRegisterMode(event: boolean) {
